b406a69ede71da1bd054994a7b419b788d11ecc8
ofird <ofirdagan@gmail.com>
2014-04-07 20:35:48 +0300

    first commit

---
+++ scripts/post-message-api.js
@@ -0,0 +1,14 @@
+/**
+ * Created by dagan on 07/04/2014.
+ */
+'use strict';
+
+window.addEventListener("message", message, false);
+function receiveMessage(event) {
+
+  if (!event.origin.match(/[http|https]:\/\/.*wix.*.com/)) {
+    return;
+  } else {
+     debugger;
+  }
+}
