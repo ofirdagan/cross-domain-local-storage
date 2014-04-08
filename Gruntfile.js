48c2fa3cbff61b41ebc4796418c4a3c0232f6334
Ofir Dagan <ofird@wix.com>
2014-04-08 16:51:45 +0300

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+'use strict';
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-release');
+  grunt.loadNpmTasks('grunt-karma');
+
+  grunt.initConfig({
+    karma: {
+      options: {
+        configFile: 'karma.conf.js'
+      },
+      ci: {
+        singleRun: true,
+        preprocessors: {'*.js': 'coverage'},
+        reporters: ['progress', 'coverage'],
+        coverageReporter: {dir : 'coverage/', type: 'lcov'}
+      },
+      dev: {
+        background: true
+      }
+    },
+    connect: {
+      options: {
+        livereload: true,
+        port: 9000,
+        open: 'http://localhost:<%= connect.options.port %>/demo/dummy-client.html'
+      },
+      server: {
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      tests: {
+        files: ['*.js', 'test/**/*.js', '{demo,css,images}/*.*'],
+        tasks: ['karma:dev:run']
+      }
+    },
+    release: {
+      options: {
+        file: 'bower.json',
+        npm: false
+      }
+    }
+  });
+
+  //run tests only once (continuous integration mode)
+  grunt.registerTask('test', ['karma:ci']);
+
+  //to debug tests during 'grunt serve', open: http://localhost:8880/debug.html
+  grunt.registerTask('serve', ['karma:dev', 'connect', 'watch']);
+};
